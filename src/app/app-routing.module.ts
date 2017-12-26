6e435a14fd306800f131ad49490954b273b58a36
xieshundaa <17743566997@163.com>
2017-12-26 13:43:17 +0800

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
 
-import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes =[
+const routes: Routes = [
   {
     path: '',
     redirectTo: '/dashboard',
     pathMatch: 'full'
   },
   {
-    path:'heroes',
+    path: 'heroes',
     component: HeroesComponent
   },
   {
@@ -23,13 +23,9 @@ const routes: Routes =[
     path: 'dashboard',
     component: DashboardComponent
   }
-]
+];
 @NgModule({
-  imports:[
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
